ff2a9d059ff54806edd54a01447a7b4321b3981b
Sameer <SameerMMurtaja@gmail.com>
2023-05-08 21:36:51 +0300

    add task screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_final/screens/add_task_screen.dart';
 import 'package:flutter_final/screens/home_screen.dart';
 
 void main() {
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: AddTaskScreen(),
     );
   }
 }
--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,96 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:test_final/helpers/database_helper.dart';
-import 'package:test_final/models/task_model.dart';
 import 'home_screen.dart';
-import 'package:intl/intl.dart';
-import 'package:toast/toast.dart';
 
 class AddTaskScreen extends StatefulWidget {
-  final Function updateTaskList;
-  final Task task;
-
-  AddTaskScreen({this.updateTaskList, this.task});
 
   @override
   _AddTaskScreenState createState() => _AddTaskScreenState();
 }
 
 class _AddTaskScreenState extends State<AddTaskScreen> {
-  final _formKey = GlobalKey<FormState>();
   String _title = '';
-  String _priority;
+  String? _priority;
   DateTime _date = DateTime.now();
   TextEditingController _dateController = TextEditingController();
-  final DateFormat _dateFormatter = DateFormat('MMM dd, yyyy');
 
   final List<String> _priorities = ['Low', 'Medium', 'High'];
-
-  @override
-  void initState() {
-    super.initState();
-
-    if (widget.task != null) {
-      _title = widget.task.title;
-      _date = widget.task.date;
-      _priority = widget.task.priority;
-    }
-
-    _dateController.text = _dateFormatter.format(_date);
-  }
-
-  @override
-  void dispose() {
-    _dateController.dispose();
-    super.dispose();
-  }
-
-  _handleDatePicker() async {
-    final DateTime date = await showDatePicker(
-      context: context,
-      initialDate: _date,
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2100),
-    );
-    if (date != null && date != _date) {
-      setState(() {
-        _date = date;
-      });
-      _dateController.text = _dateFormatter.format(date);
-    }
-  }
-
-  _delete() {
-    DatabaseHelper.instance.deleteTask(widget.task.id);
-    Navigator.pop(context);
-    widget.updateTaskList();
-    Toast.show("Task Deleted", context,
-        duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
-  }
-
-  _submit() {
-    if (_formKey.currentState.validate()) {
-      _formKey.currentState.save();
-      print('$_title, $_date, $_priority');
-
-      Task task = Task(title: _title, date: _date, priority: _priority);
-      if (widget.task == null) {
-        // Insert the task to our user's database
-        task.status = 0;
-        DatabaseHelper.instance.insertTask(task);
-        Toast.show("New Task Added", context,
-            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
-      } else {
-        // Update the task
-        task.id = widget.task.id;
-        task.status = widget.task.status;
-        DatabaseHelper.instance.updateTask(task);
-        Toast.show("Task Updated", context,
-            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
-      }
-      Navigator.push(context, MaterialPageRoute(builder: (_) => HomeScreen()));
-      widget.updateTaskList();
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -105,7 +28,7 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
             onPressed: () => Navigator.pop(context)),
         title: Row(children: [
           Text(
-            widget.task == null ? 'Add Task' : 'Update Task',
+            'Add Task',
             style: const TextStyle(
               color: Colors.redAccent,
               fontSize: 20.0,
@@ -113,14 +36,6 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
             ),
           ),
         ]),
-        // actions: [
-        //   IconButton(
-        //       icon: Icon(
-        //         Icons.info_outline,
-        //         color: Colors.black,
-        //       ),
-        //       onPressed: () {}),
-        // ],
         centerTitle: false,
         elevation: 0,
       ),
@@ -133,7 +48,6 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Form(
-                  key: _formKey,
                   child: Column(
                     children: <Widget>[
                       Padding(
@@ -147,10 +61,10 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                               borderRadius: BorderRadius.circular(10.0),
                             ),
                           ),
-                          validator: (input) => input.trim().isEmpty
+                          validator: (input) => input?.trim().isEmpty??false
                               ? 'Please enter a task title'
                               : null,
-                          onSaved: (input) => _title = input,
+                          onSaved: (input) => _title = input??"",
                           initialValue: _title,
                         ),
                       ),
@@ -160,7 +74,6 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                           readOnly: true,
                           controller: _dateController,
                           style: TextStyle(fontSize: 18.0),
-                          onTap: _handleDatePicker,
                           decoration: InputDecoration(
                             labelText: 'Date',
                             labelStyle: TextStyle(fontSize: 18.0),
@@ -216,40 +129,18 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                           color: Theme.of(context).primaryColor,
                           borderRadius: BorderRadius.circular(30.0),
                         ),
-                        // ignore: deprecated_member_use
                         child: ElevatedButton(
+                          onPressed: () {  },
                           child: Text(
-                            widget.task == null ? 'Add' : 'Update',
+                            'Add',
                             style: TextStyle(
                               color: Colors.white,
                               fontSize: 20.0,
                             ),
                           ),
-                          onPressed: _submit,
                         ),
                       ),
-                      widget.task != null
-                          ? Container(
-                              margin: EdgeInsets.symmetric(vertical: 0.0),
-                              height: 60.0,
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                color: Theme.of(context).primaryColor,
-                                borderRadius: BorderRadius.circular(30.0),
-                              ),
-                        // ignore: deprecated_member_use
-                              child: ElevatedButton(
-                                child: Text(
-                                  'Delete',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20.0,
-                                  ),
-                                ),
-                                onPressed: _delete,
-                              ),
-                            )
-                          : SizedBox.shrink(),
+                      SizedBox.shrink(),
                     ],
                   ),
                 ),
