b1b55f2683c354ae401ec69ac6fa2fa12e4c2722
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-05-09 14:56:19 +0200

    add item design for tasks and categories

---
+++ lib/ui/item_category.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:to_do_app/home_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  Category category;
+  CategoryItem(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(category.name, style: TextStyle(fontSize: 18)),
+      trailing: Icon(Icons.arrow_forward_ios),
+      onTap: () {
+        // TODO: Add navigation logic
+      },
+      contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      tileColor: Colors.white,
+    );
+  }
+}
---
+++ lib/ui/item_task.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+class TaskItem extends StatelessWidget {
+  Task task;
+  TaskItem(this.task);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(task.title, style: TextStyle(fontSize: 18)),
+      subtitle: Text(task.description,
+          overflow: TextOverflow.ellipsis, style: TextStyle(fontSize: 14)),
+      trailing: Icon(Icons.arrow_forward_ios),
+      onTap: () {
+        // TODO: Add navigation logic
+      },
+      contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      tileColor: Colors.white,
+    );
+  }
+}
